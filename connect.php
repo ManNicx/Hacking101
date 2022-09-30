b035c31a745858b2c945c06fa59266e8537fc665
ManNicx <113735662+ManNicx@users.noreply.github.com>
2022-09-30 14:33:22 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,31 @@
+<?php  
+ // session will save your data inside your machine 
+ session_start(); 
+ // function of our database connection 
+ function connection(){ 
+     // the name of the server 
+     $servername ='localhost'; 
+     //username of phpmyadmi 
+     $username ='admin'; 
+     //windows user doesnt have password 
+     //for mac user: password is root 
+     $password ='admin'; 
+     //the database name created in phpmyadmin 
+     $database ='Registration'; 
+  
+     // a variable that holds the connection between php and phpmyadmin 
+     $conn = new mysqli($servername,$username,$password,$database); 
+  
+     // displays error if there is any 
+     if($conn->connect_error){ 
+         die("Connection error in ".$conn->connect_error); 
+     }else{ 
+         // returns the connection to the function for future uses 
+         return $conn; 
+         // echo "connection successful"; 
+     } 
+  
+ } 
+  
+ // echo connection();
+?>
